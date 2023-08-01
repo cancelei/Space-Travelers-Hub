566c0118a4eaa92880e13c205c616c6468759b00
Glauber <glauber.bannwart@gmail.com>
2023-07-31 21:33:03 -0300

    Nav console error fix - activeclassname

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -10,13 +10,13 @@ function NavBar() {
         <h1>Space Travelers Hub</h1>
         <ul>
           <li>
-            <NavLink to="/" activeClassName="active-link">Rockets</NavLink>
+            <NavLink to="/" activeclassname="active-link">Rockets</NavLink>
           </li>
           <li>
-            <NavLink to="/missions" activeClassName="active-link">Missions</NavLink>
+            <NavLink to="/missions" activeclassname="active-link">Missions</NavLink>
           </li>
           <li>
-            <NavLink to="/myprofile" activeClassName="active-link">My profile</NavLink>
+            <NavLink to="/myprofile" activeclassname="active-link">My profile</NavLink>
           </li>
         </ul>
       </div>
