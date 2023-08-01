b7d108a40a39f215944857158f4541c1618ecdc7
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-08-01 12:54:29 -0600

    Reorder nav sections

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -16,10 +16,10 @@ function NavBar() {
             <NavLink to="/missions" activeClassname="active-link">Missions</NavLink>
           </li>
           <li>
-            <NavLink to="/myprofile" activeClassname="active-link">My profile</NavLink>
+            <NavLink to="/dragons" activeClassName="active-link">Dragons</NavLink>
           </li>
           <li>
-            <NavLink to="/dragons" activeClassName="active-link">Dragons</NavLink>
+            <NavLink to="/myprofile" activeClassname="active-link">My profile</NavLink>
           </li>
 
         </ul>
