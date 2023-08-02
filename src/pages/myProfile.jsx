8e16d792546ddeb56175b2c57e3bc6d2bec6a16a
rudicarrilloypr <125106965+rudicarrilloypr@users.noreply.github.com>
2023-08-02 13:30:25 -0600

    Add dragons reservations to myProfile

--- src/pages/myProfile.jsx
+++ src/pages/myProfile.jsx
@@ -4,8 +4,10 @@ import Table from 'react-bootstrap/Table';
 
 const MyProfile = () => {
   const missions = useSelector((state) => state.missions.missions);
+  const dragons = useSelector((state) => state.dragons.list);
 
   const joinedMissions = missions.filter((mission) => mission.reserved);
+  const reservedDragons = dragons.filter((dragon) => dragon.reserved);
 
   return (
     <div style={{ width: '90%', margin: '0 5% 0 5%' }}>
@@ -13,13 +15,29 @@ const MyProfile = () => {
         <Table striped bordered hover>
           <thead>
             <tr>
-              <th>Joined Missions</th>
+              <th>My Missions</th>
             </tr>
           </thead>
           <tbody>
             {joinedMissions.map((mission) => (
               <tr key={mission.mission_id}>
                 <td>{mission.mission_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+      <div style={{ width: '45%', marginTop: '20px' }}>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>My Dragons</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reservedDragons.map((dragon) => (
+              <tr key={dragon.dragonId}>
+                <td>{dragon.dragonName}</td>
               </tr>
             ))}
           </tbody>
