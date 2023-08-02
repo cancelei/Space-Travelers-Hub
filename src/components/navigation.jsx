05ee53b5d1593f747f3c7bbbb61663a79fc2b935
Glauber <glauber.bannwart@gmail.com>
2023-08-02 12:32:15 -0300

    Update Navigation Component

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,25 +1,27 @@
 import { NavLink } from 'react-router-dom';
 import logo from '../assets/logo.png';
-import '../styles/navigation.module.css';
+import '../styles/navigation.css';
 
 function NavBar() {
   return (
     <nav>
-      <div>
-        <img src={logo} alt="Space Travelers Hub" />
-        <h1>Space Travelers Hub</h1>
-        <ul>
+      <div className="parentcontainer300">
+        <div className="container300">
+          <img className="logo300" src={logo} alt="Space Travelers Hub" />
+          <h1>Space Travelers Hub</h1>
+        </div>
+        <ul className="navlinkcontainer300">
           <li>
-            <NavLink to="/" activeclassname="active-link">Rockets</NavLink>
+            <NavLink to="/" className="navlink300" activeclassname="active-link">Rockets</NavLink>
           </li>
           <li>
-            <NavLink to="/missions" activeclassname="active-link">Missions</NavLink>
+            <NavLink to="/missions" className="navlink300" activeclassname="active-link">Missions</NavLink>
           </li>
           <li>
-            <NavLink to="/dragons" activeclassname="active-link">Dragons</NavLink>
+            <NavLink to="/dragons" className="navlink300" activeclassname="active-link">Dragons</NavLink>
           </li>
           <li>
-            <NavLink to="/myprofile" activeclassname="active-link">My profile</NavLink>
+            <NavLink to="/myprofile" className="navlink300" activeclassname="active-link">My profile</NavLink>
           </li>
 
         </ul>
